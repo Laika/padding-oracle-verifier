5d13e7e6116c69120111b04ca04160fd954b9d4e
Laika <laika@albina.cc>
2022-06-29 01:19:33 +0900

    Add some features for encryption attack

--- app/server.py
+++ app/server.py
@@ -7,7 +7,7 @@ from Crypto.Util.Padding import pad, unpad
 def main():
     flag = os.getenvb(b"FLAG", b"FAKE{REDACTED}")
     key = os.urandom(16)
-    iv = os.urandom(16)
+    iv = os.urandom(AES.block_size)
 
     cipher = AES.new(key=key, mode=AES.MODE_CBC, iv=iv)
 
@@ -16,13 +16,15 @@ def main():
     print(iv.hex())
 
     while True:
-        cipher = AES.new(key=key, mode=AES.MODE_CBC, iv=iv)
         try:
             user_input = bytes.fromhex(input("> ").strip())
-            unpad(cipher.decrypt(user_input), AES.block_size)
+            iv, ciphertext = user_input[:AES.block_size], user_input[AES.block_size:]
+            cipher = AES.new(key=key, mode=AES.MODE_CBC, iv=iv)
+            plaintext = unpad(cipher.decrypt(user_input), AES.block_size)
             print("ok")
+            print(f"{plaintext = }")
         except Exception as e:
-            print(f"error: {e}")
+            print(f"error: {e}, {key}, {iv}")
 
 
 if __name__ == "__main__":
