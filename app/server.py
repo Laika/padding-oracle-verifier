44d24c32b6b7b41226374eebb31cc745c560f7ae
Laika <laika@albina.cc>
2022-06-28 20:22:28 +0900

    Update

--- app/server.py
+++ app/server.py
@@ -5,24 +5,24 @@ from Crypto.Util.Padding import pad, unpad
 
 
 def main():
-    flag = os.getenv("FLAG", "FAKE{REDACTED}").encode()
+    flag = os.getenvb(b"FLAG", b"FAKE{REDACTED}")
     key = os.urandom(16)
     iv = os.urandom(16)
 
     cipher = AES.new(key=key, mode=AES.MODE_CBC, iv=iv)
 
-    ciphertext = cipher.encrypt(pad(flag, AES.block_size))
-    print(f"{iv = }")
-    print(f"{ciphertext = }")
-
-    cipher = AES.new(key=key, mode=AES.MODE_CBC, iv=iv)
+    ciphertext = cipher.encrypt(pad(flag, AES.block_size)).hex()
+    print(ciphertext)
+    print(iv.hex())
 
     while True:
+        cipher = AES.new(key=key, mode=AES.MODE_CBC, iv=iv)
         try:
-            user_input = bytes.fromhex(input("> "))
-            print(unpad(cipher.decrypt(user_input), AES.block_size))
+            user_input = bytes.fromhex(input("> ").strip())
+            unpad(cipher.decrypt(user_input), AES.block_size)
+            print("ok")
         except Exception as e:
-            print(e)
+            print(f"error: {e}")
 
 
 if __name__ == "__main__":
